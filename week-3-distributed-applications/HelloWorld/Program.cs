c6ae68edb77e23982b7009d93693ea9444c83246
Jack Bailey <jbailey@Steve-Macbook-Pro-2019.local>
2022-10-10 14:48:45 +0100

    Created CSharp Base Files

---
+++ week-3-distributed-applications/HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace WelcomeToCSharp
+class Helloworld{
+    static void Main(String[] args){
+        Console.WriteLine("Hello, World!");
+        Console.Read();
+    }
+}
