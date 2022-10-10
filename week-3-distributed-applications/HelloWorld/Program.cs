d716a58db5d1205d9580fe0f5bbd156292257590
Jack Bailey <jbailey@Steve-Macbook-Pro-2019.local>
2022-10-10 15:12:58 +0100

    Getting User Input example added

--- week-3-distributed-applications/HelloWorld/Program.cs
+++ week-3-distributed-applications/HelloWorld/Program.cs
@@ -1,9 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 using System;
-namespace WelcomeToCSharp
-class Helloworld{
-    static void Main(String[] args){
-        Console.WriteLine("Hello, World!");
-        Console.Read();
+
+namespace WelcomeToCSharp; // A namespace allows the seperation of code. They are used for creating a logical seperation between parts of code
+
+class Helloworld{ // Everything in C Sharp must be caontined within a class, as it is a purely object-oriented programming language
+
+    static void Main(String[] args){ // This is our programs entry point
+
+        Console.WriteLine("Hello, World!"); // Allows writing to the console
     }
+
 }
---
+++ week-3-distributed-applications/getting-input/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace GettingInput; // A namespace allows the seperation of code. They are used for creating a logical seperation between parts of code
+
+class GettingInput{ // Everything in C Sharp must be caontined within a class, as it is a purely object-oriented programming language
+
+    static void Main(String[] args){ // This is our programs entry point
+
+        Console.WriteLine("Please enter your First name and press ENTER"); // Allows writing to the console
+        String fname = Console.ReadLine();
+        Console.WriteLine("Please enter your middle name and press ENTER"); // Allows writing to the console
+        String mname = Console.ReadLine();
+        Console.WriteLine("Please enter your last name and press ENTER"); // Allows writing to the console
+        String lname = Console.ReadLine();
+        Console.WriteLine(fname + " " + mname + " " + lname);
+    }
+}
