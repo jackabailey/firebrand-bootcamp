78a362825f4c1e177c0e33ff8731114260943be9
Jack Bailey <jbailey@Aurora-Macbook-Air-2020-M1.local>
2022-10-11 09:45:04 +0100

    Rename Files

---
+++ weekThreeDistributedApplications/csProgramStructure/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace WelcomeToCSharp; // A namespace allows the seperation of code. They are used for creating a logical seperation between parts of code
+
+class Helloworld{ // Everything in C Sharp must be caontined within a class, as it is a purely object-oriented programming language
+
+    static void Main(String[] args){ // This is our programs entry point
+    // It takes an array of strings. This is what is read when we provide build or run tags in command line.
+    // A CS5001 Error will be thrown by the terminal if this is the case
+
+        Console.WriteLine("Hello, World!"); // Allows writing to the console
+    }
+
+}
