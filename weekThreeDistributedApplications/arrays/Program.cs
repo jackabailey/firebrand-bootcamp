d617764dcb6f77e0f1177afe52b67b0743924d74
jackabailey <services@bailey.ws>
2022-10-12 12:02:27 +0100

    Demonstrated arrays in C#

---
+++ weekThreeDistributedApplications/arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] numbers = new int[10]; // all ten zeros            numbers[5] = 15;
+            numbers[9] = 23;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("index is {0} value {1}",i,numbers[i]);
+            }
+            float[] floats = new float[10];
+            for (int i = 0; i < floats.Length; i++)
+            {
+                Console.WriteLine("index is {0} value {1}", i, floats[i]);
+            }
+            double[] doubles = new double[10];
+            for (int i = 0; i < doubles.Length; i++)
+            {
+                Console.WriteLine("index is {0} value {1}", i, doubles[i]);
+            }
