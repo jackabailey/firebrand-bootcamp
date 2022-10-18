b986e4e35a874d1812091cdb60b45282971049f6
Jack Bailey <services@bailey.ws>
2022-10-18 14:14:20 +0100

    Created basic calculator with addition

---
+++ weekFourProjects/CSCalculator/Mathematics.cs
@@ -0,0 +1,8 @@
+namespace Calculator {
+    class Mathematics {
+        public int Addition(int numberOne, int numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+    }
+}
--- weekFourProjects/CSCalculator/Program.cs
+++ weekFourProjects/CSCalculator/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Calculator {
+    class Program {
+        static void Main(string[] args)
+        {
+            Mathematics example = new Mathematics();
+            Console.WriteLine(example.Addition(1, 2));
+        }
+    }
+}
